88419cf7d489f31785a31c96791cb422a4f5803e
Mrshivam09 <125801826+Mrshivam09@users.noreply.github.com>
2026-06-02 16:55:47 +0530

    Update SearchPage.java

--- src/test/java/Pages/SearchPage.java
+++ src/test/java/Pages/SearchPage.java
@@ -1,30 +1,45 @@
 package Pages;
 
+import java.time.Duration;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class SearchPage {
-
 
     WebDriver driver;
 
     public SearchPage(WebDriver driver) {
-
         this.driver = driver;
     }
 
     By search = By.name("search");
 
     By searchBtn =
-    By.xpath("//button[contains(@class,'btn-default')]");
+        By.xpath("//button[contains(@class,'btn-default')]");
 
     public void searchProduct(String product) {
-
+        driver.findElement(search).clear();
         driver.findElement(search).sendKeys(product);
     }
 
     public void clickSearchButton() {
 
-        driver.findElement(searchBtn).click();
+        WebDriverWait wait =
+            new WebDriverWait(driver, Duration.ofSeconds(10));
+
+        WebElement button =
+            wait.until(ExpectedConditions.elementToBeClickable(searchBtn));
+
+        try {
+            button.click();
+        } catch (Exception e) {
+            ((JavascriptExecutor) driver)
+                .executeScript("arguments[0].click();", button);
+        }
     }
 }
